bb70e1a5f8240e15384277f001dd4fe3c7cde04c
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2020-12-17 23:25:28 +0000

    files are added, team avg scores calculated

---
+++ script.js
@@ -0,0 +1,10 @@
+let teamJohnAverage = Math.round((89 + 120 + 103) / 3);
+let teamMikeAverage = Math.round((116 + 94 + 123) / 3);
+
+if (teamJohnAverage > teamMikeAverage) {
+	console.log(`John's team wins with ${teamJohnAverage} points.`);
+} else if (teamJohnAverage < teamMikeAverage) {
+	console.log(`Mike's team wins with ${teamMikeAverage} points.`);
+} else {
+	console.log(`There is draw. Both teams got ${teamMikeAverage} points`);
+}
