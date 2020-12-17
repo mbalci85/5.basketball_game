4c81ecac9d9e50b63ff11e409d8f247730a49c12
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2020-12-17 23:58:15 +0000

    new team added

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-let teamJohnAverage = Math.round((119 + 120 + 103) / 3);
-let teamMikeAverage = Math.round((116 + 94 + 123) / 3);
+let teamJohnAverage = Math.round((124 + 120 + 103) / 3);
+let teamMikeAverage = Math.round((129 + 120 + 103) / 3);
+let teamMaryAverage = Math.round((103 + 104 + 145) / 3);
 
-if (teamJohnAverage > teamMikeAverage) {
-	console.log(`John's team wins with ${teamJohnAverage} points.`);
-} else if (teamJohnAverage < teamMikeAverage) {
+// if (teamJohnAverage > teamMikeAverage) {
+// 	console.log(`John's team wins with ${teamJohnAverage} points.`);
+// } else if (teamJohnAverage < teamMikeAverage) {
+// 	console.log(`Mike's team wins with ${teamMikeAverage} points.`);
+// } else {
+// 	console.log(`There is draw. Both teams got ${teamMikeAverage} points`);
+// }
+
+console.log("John's team: " + teamJohnAverage);
+console.log("Mike's team: " + teamMikeAverage);
+console.log("Mary's team: " + teamMaryAverage);
+
+if (teamMikeAverage > teamMaryAverage && teamMikeAverage > teamJohnAverage) {
 	console.log(`Mike's team wins with ${teamMikeAverage} points.`);
-} else {
-	console.log(`There is draw. Both teams got ${teamMikeAverage} points`);
+} else if (
+	teamMaryAverage > teamMikeAverage &&
+	teamMaryAverage > teamJohnAverage
+) {
+	console.log(`Mary's team wins with ${teamMaryAverage} points.`);
+} else if (
+	teamJohnAverage > teamMikeAverage &&
+	teamJohnAverage > teamMaryAverage
+) {
+	console.log(`John's team wins with ${teamJohnAverage} points.`);
+} else if (
+	teamJohnAverage == teamMikeAverage &&
+	teamJohnAverage == teamMaryAverage
+) {
+	console.log(`There is draw and all teams scored ${teamJohnAverage} points.`);
+} else if (
+	teamJohnAverage == teamMikeAverage &&
+	teamJohnAverage > teamMaryAverage
+) {
+	console.log(
+		`There is draw between John's and Mike's team with ${teamJohnAverage} points.`,
+	);
+} else if (
+	teamMaryAverage == teamMikeAverage &&
+	teamMaryAverage > teamJohnAverage
+) {
+	console.log(
+		`There is draw between Mary's and Mike's team with ${teamMaryAverage} points.`,
+	);
+} else if (
+	teamMaryAverage == teamJohnAverage &&
+	teamMaryAverage > teamMikeAverage
+) {
+	console.log(
+		`There is draw between Mary's and John's team with ${teamMaryAverage} points.`,
+	);
 }
